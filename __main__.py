7e75213d37b74dfa1c05a0b35497d3a57d2b22b9
Ed Kerstetter <ekerstetter@burwood.com>
2023-01-16 12:56:19 -0600

    Seems to be working

--- __main__.py
+++ __main__.py
@@ -1,7 +1,48 @@
 import requests
+from pathlib import PurePath
+
 
 def main():
-    pass
+    username = "ECP"
+    password = "temp_password"
+    core_IP = "192.168.180.151"
+    cred_json = {
+        "username": username,
+        "password": password
+    }
 
-if __name__ == "__namme__":
+    # Get Token
+    r = requests.post(f"https://{core_IP}/api/v0/logon",
+                      json=cred_json, verify=False)
+    token = r.json()["token"]
+
+    # List All Recordings in Directory
+    headers_json = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/json"
+    }
+
+    r = requests.get(
+        f"https://{core_IP}/api/v0/cores/self/media/Recordings", headers=headers_json, verify=False)
+    list_of_recordings = [sub['path'] for sub in r.json()]
+    headers_json = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/octet-stream"
+    }
+    headers_delete_json = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    # Loop through list_of_recordings and download, then delete files.
+    for file in list_of_recordings:
+
+        with requests.get(f"https://{core_IP}/api/v0/cores/self/media/{file}", headers=headers_json, verify=False, stream=True) as r:
+            with open(PurePath(file), 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        requests.delete(
+            f"https://{core_IP}/api/v0/cores/self/media/{file}", headers=headers_delete_json, verify=False)
+
+
+if __name__ == "__main__":
     main()
